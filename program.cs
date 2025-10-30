725095d55bce7bb77b8d601b06a7b4638109bf72
Bhawna <bhawnaa.066@gmail.com>
2025-10-30 19:55:50 +0530

    Add files via upload

---
+++ program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Project__6_Digit_OTP_Generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int otp = random.Next(100000, 999999);
+            Console.WriteLine($" Your OTP is {otp}. Do not share it with anyone.");
+        }
+    }
+}
